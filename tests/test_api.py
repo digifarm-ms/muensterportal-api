e27d07f965d103d2a42d6f21f0cd4c0fc34df72a
Alexander Milden <milden@westphalia-datalab.com>
2026-05-17 23:53:59 +0200

    add happy path test for search endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,3 +19,11 @@ def test_client(retriever_with_pages: LanceDBRetriever) -> Iterator[TestClient]:
 
 def test_smoke_test_test_client(test_client: TestClient) -> None:
     pass
+
+
+def test_search_endpoint(test_client: TestClient) -> None:
+    params = {"query": "Wieso ist Münster cool?"}
+
+    response = test_client.get("/search", params=params)
+
+    assert len(response.json()["results"]) == 2
